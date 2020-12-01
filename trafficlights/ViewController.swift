48dfeb8a8fa6fe61d5b3396190424c0a53eda189
Kuzma Kulishin <kuzma.zzz@gmail.com>
2020-12-01 20:07:16 +0300

    add outlet and button connection

--- trafficlights/ViewController.swift
+++ trafficlights/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLigth: UIView!
+    @IBOutlet var greenLigth: UIView!
+    @IBOutlet var buttonStatus: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startButton(_ sender: UIButton) {
+    }
+    
 }
 
