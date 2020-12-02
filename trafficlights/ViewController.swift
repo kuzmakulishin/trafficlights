227755ae073be49a4f817265e36ecf32bd88c114
Kuzma Kulishin <kuzma.zzz@gmail.com>
2020-12-02 13:46:46 +0300

    add switch color

--- trafficlights/ViewController.swift
+++ trafficlights/ViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
     
     @IBOutlet var redLight: UIView!
@@ -14,13 +16,64 @@ class ViewController: UIViewController {
     @IBOutlet var greenLigth: UIView!
     @IBOutlet var buttonStatus: UIButton!
     
-
+    enum ChangeColor {
+        case off, red, yellow, green
+    }
+    
+    var changeColor = ChangeColor.off
+    
+    
+    let lightOn: CGFloat = 1
+    let lightOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        redLight.alpha = lightOff
+        yellowLigth.alpha = lightOff
+        greenLigth.alpha = lightOff
+        
+        redLight.layer.cornerRadius = redLight.layer.frame.height / 2
+        yellowLigth.layer.cornerRadius = yellowLigth.layer.frame.height / 2
+        greenLigth.layer.cornerRadius = greenLigth.layer.frame.height / 2
+        buttonStatus.layer.cornerRadius = 15
+        
     }
 
     @IBAction func startButton(_ sender: UIButton) {
+        
+        
+        switch changeColor {
+        case .off:
+            redLight.alpha = lightOn
+            changeColor = .red
+            buttonStatus.setTitle("Next yellow", for: .normal)
+            buttonStatus.setTitleColor(UIColor.systemYellow, for: .normal)
+        case .red:
+            redLight.alpha = lightOff
+            yellowLigth.alpha = lightOn
+            greenLigth.alpha = lightOff
+            changeColor = .yellow
+            buttonStatus.setTitle("Next green", for: .normal)
+            buttonStatus.setTitleColor(UIColor.systemGreen, for: .normal)
+        case .yellow:
+            redLight.alpha = lightOff
+            yellowLigth.alpha = lightOff
+            greenLigth.alpha = lightOn
+            changeColor = .green
+            buttonStatus.setTitle("Next red", for: .normal)
+            buttonStatus.setTitleColor(UIColor.systemRed, for: .normal)
+        case .green:
+            yellowLigth.alpha = lightOff
+            redLight.alpha = lightOn
+            greenLigth.alpha = lightOff
+            changeColor = .red
+            buttonStatus.setTitle("Next yellow", for: .normal)
+            buttonStatus.setTitleColor(UIColor.systemYellow, for: .normal)
+            
+        }
+        
     }
     
 }
